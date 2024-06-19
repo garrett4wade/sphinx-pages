de59acacf77d3077bb80a8298274c826970ceb93
Wei Fu <36355462+garrett4wade@users.noreply.github.com>
2024-06-19 11:30:01 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -102,6 +102,8 @@ echo ::endgroup::
 # sphinx extensions
 if [ "$INPUT_INSTALL_EXTENSIONS" = true ] ; then
     echo ::group::Installing sphinx extensions
+    echo "Update PyPI"
+    pip3 install -U pip
     echo "pip3 install -r $docs_src/$INPUT_SOURCE_DIR/requirements.txt"
     pip3 install -r $docs_src/$INPUT_SOURCE_DIR/requirements.txt
     echo ::endgroup::
